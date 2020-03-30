fb4e3bdf1be35baf0d4a21ba1f8da245a1fb86db
Max Boguslavskiy <max.bogus@gmail.com>
2020-03-31 01:57:24 +0300

    added feature scaling example

---
+++ feature_scaling.py
@@ -0,0 +1,20 @@
+import pandas
+import numpy
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+
+# Assign the data to predictor and outcome variables
+train_data = pandas.read_csv('data.csv', header = None)
+X = train_data.iloc[:,:-1]
+y = train_data.iloc[:,-1]
+
+scaler = StandardScaler()
+
+X_scaled = scaler.fit_transform(X)
+
+lasso_reg = Lasso()
+
+lasso_reg.fit(X_scaled, y)
+
+reg_coef = lasso_reg.coef_
+print(reg_coef)
